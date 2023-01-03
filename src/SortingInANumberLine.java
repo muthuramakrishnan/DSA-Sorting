9993f76ac2f0edb2c78bac1ee951468fba7c41d1
Muthuramakrishnan M <muthu.rama.krishnan96@gmail.com>
2023-01-03 22:24:14 +0530

    code format

--- src/SortingInANumberLine.java
+++ src/SortingInANumberLine.java
@@ -4,18 +4,18 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class SortingInANumberLine {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] s = br.readLine().split(" ");
-        int n = s.length;
-        List<Map.Entry<Integer, Integer>> points = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            int a = Integer.parseInt(s[i].split(",")[0]);
-            int b = Integer.parseInt(s[i].split(",")[1]);
-            points.add(new AbstractMap.SimpleEntry<>(a, b));
-        }
-        System.out.println(points);
-        Collections.sort(points, new PointDistanceFromOriginComparator());
-        System.out.println(points);
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String[] s = br.readLine().split(" ");
+    int n = s.length;
+    List<Map.Entry<Integer, Integer>> points = new ArrayList<>();
+    for (int i = 0; i < n; i++) {
+      int a = Integer.parseInt(s[i].split(",")[0]);
+      int b = Integer.parseInt(s[i].split(",")[1]);
+      points.add(new AbstractMap.SimpleEntry<>(a, b));
     }
+    System.out.println(points);
+    Collections.sort(points, new PointDistanceFromOriginComparator());
+    System.out.println(points);
+  }
 }
