82c6676e09e1e076a50f333cae420f7d20d88e4f
Muthuramakrishnan M <muthu.rama.krishnan96@gmail.com>
2023-01-03 22:21:02 +0530

    Initial commit

---
+++ src/PointDistanceFromOriginComparator.java
@@ -0,0 +1,16 @@
+import java.util.Comparator;
+import java.util.Map;
+
+public class PointDistanceFromOriginComparator implements Comparator<Map.Entry<Integer, Integer>> {
+    public int compare(Map.Entry<Integer, Integer> x, Map.Entry<Integer, Integer> y) {
+        int x1 = x.getKey();
+        int y1 = x.getValue();
+        int x2 = y.getKey();
+        int y2 = y.getValue();
+
+        int d1 = x1 * x1 + y1 * y1;
+        int d2 = x2 * x2 + y2 * y2;
+
+        return d1 - d2;
+    }
+}
---
+++ src/SortingInANumberLine.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class SortingInANumberLine {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        int n = s.length;
+        List<Map.Entry<Integer, Integer>> points = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int a = Integer.parseInt(s[i].split(",")[0]);
+            int b = Integer.parseInt(s[i].split(",")[1]);
+            points.add(new AbstractMap.SimpleEntry<>(a, b));
+        }
+        System.out.println(points);
+        Collections.sort(points, new PointDistanceFromOriginComparator());
+        System.out.println(points);
+    }
+}
