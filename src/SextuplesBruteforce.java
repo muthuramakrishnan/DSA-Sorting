bf7a4849891f9cb96828a4ab49ac3515903ad531
Muthuramakrishnan M <muthu.rama.krishnan96@gmail.com>
2023-01-09 22:21:40 +0530

    sextuple bruteforce

---
+++ src/SextuplesBruteforce.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SextuplesBruteforce {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        int n = s.length;
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(s[i]);
+        }
+
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                for(int k=0; k<n; k++){
+                    for(int l=0; l<n; l++){
+                        for(int m=0; m<n; m++){
+                            for(int o=0; o<n; o++){
+                                int lhs = ((arr[i] * arr[j] + arr[k]) / arr[l]) - arr[m];
+                                if(lhs == arr[o]){
+                                    ans++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(ans);
+    }
+}
